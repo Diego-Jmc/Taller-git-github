c3799a62c832953f76f484abf7f4abbc009f35ef
leiver valenciano <leiver.valenciano@gmail.com>
2023-08-19 17:40:14 -0600

    Se unieron todas las operaciones

--- Calculadora.cpp
+++ Calculadora.cpp
@@ -24,7 +24,27 @@ double division(int a, int c) {
 
 int main(int argc, char *argv[]) {
 	
+	int a,b, opc;
 	
+	cout<<"Ingrese 2 valores para hacer una operacion"<<endl;
+	cin>>a>>b;
+	cout<<"Cual operacion desea hacer: "<<endl;
+	cout<<"1. Suma"<<endl;
+	cout<<"2. resta"<<endl;
+	cout<<"3. multiplicacion"<<endl;
+	cout<<"4. division"<<endl;
+	cout<<endl;
+	cin>>opc;
+	
+	if(opc == 1){
+		cout<<suma(a,b);
+	}else if(opc == 2){
+		cout<<resta(a,b);
+	}else if(opc == 3){
+		cout<<multiplicacion(a,b);
+	}else if(opc == 4){
+		cout<<division(a,b);
+	}
 	
 	
 	return 0;
